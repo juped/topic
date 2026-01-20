f694fc2b5fe574f6b514534583994acbeb8aeab8
Raymond E. Pasco <ray@ameretat.dev>
2026-01-19 23:26:50 -0500

    create: implement topic dependencies

--- cmd/create.go
+++ cmd/create.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 	"topic/pkg/git"
 	"topic/pkg/topic"
 
@@ -23,8 +24,16 @@ var createCmd = &cobra.Command{
 	},
 }
 
+var dependencies []string
+
 func init() {
 	rootCmd.AddCommand(createCmd)
+	createCmd.Flags().StringArrayVarP(
+		&dependencies,
+		"depends", "d",
+		[]string{},
+		`The name of a topic branch this topic depends on.
+(Can be repeated for multiple dependencies.)`)
 }
 
 func topicCreate(cmd *cobra.Command, args []string) error {
@@ -42,19 +51,83 @@ func topicCreate(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	releaseTag, err := git.ReleaseTag(gitDir, config.BaseBranch)
+	basePoint, err := git.ReleaseTag(gitDir, config.BaseBranch)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "release tag not found, "+
 			"basing on %s instead\n", config.BaseBranch)
 		fmt.Fprintf(os.Stderr, "remember to make a release tag asap!\n")
-		releaseTag = config.BaseBranch
+		basePoint = config.BaseBranch
 	}
 
-	err = git.GitRun(gitDir, "branch", args[0], releaseTag)
-	if err != nil {
-		return err
+	if len(dependencies) == 0 {
+		err = git.GitRun(gitDir, "branch", args[0], basePoint)
+		if err != nil {
+			return err
+		}
+	} else if len(dependencies) == 1 {
+		fmt.Fprintf(os.Stderr, "only one dependency, basing directly on it\n")
+		err = git.GitRun(gitDir, "branch", args[0], dependencies[0])
+		if err != nil {
+			return err
+		}
+	} else {
+		fmt.Fprintf(os.Stderr, "multiple dependencies, merging them\n")
+		octopusBase, err := octopusMergeDependencies(gitDir, basePoint,
+			dependencies)
+		if err != nil {
+			return err
+		}
+		err = git.GitRun(gitDir, "branch", args[0], octopusBase)
+		if err != nil {
+			return err
+		}
 	}
 
-	fmt.Printf("created topic branch %s based on %s\n", args[0], releaseTag)
+	fmt.Printf("created topic branch %s based on %s", args[0], basePoint)
+	if len(dependencies) > 0 {
+		fmt.Printf(" and %d dependencies", len(dependencies))
+	}
+	fmt.Printf("\n")
 	return nil
+}
+
+func octopusMergeDependencies(gitDir string, basePoint string,
+	dependencies []string) (string, error) {
+	accumulator := basePoint
+	parents := []string{basePoint}
+
+	for _, dep := range dependencies {
+		tree, err := git.GitCommand(gitDir, "merge-tree", accumulator, dep)
+		if err != nil {
+			return "", err
+		}
+
+		parents = append(parents, dep)
+
+		commitTreeArgs := []string{"commit-tree", tree, "-m", "temporary"}
+		for _, parent := range parents {
+			commitTreeArgs = append(commitTreeArgs, "-p", parent)
+		}
+		commit, err := git.GitCommand(gitDir, commitTreeArgs...)
+		if err != nil {
+			return "", err
+		}
+		accumulator = commit
+	}
+
+	commitTreeArgs := []string{"commit-tree", accumulator + "^{tree}"}
+	for _, parent := range parents {
+		commitTreeArgs = append(commitTreeArgs, "-p", parent)
+	}
+	// todo: use fmt-merge-msg to have git generate this?
+	mergeMessage := fmt.Sprintf("Merge dependencies %s",
+		strings.Join(dependencies, ", "))
+	commitTreeArgs = append(commitTreeArgs, "-m", mergeMessage)
+
+	commit, err := git.GitCommand(gitDir, commitTreeArgs...)
+	if err != nil {
+		return "", err
+	}
+
+	return commit, nil
 }
